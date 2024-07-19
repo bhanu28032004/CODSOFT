beb130d62da269209f5b430f115c838d505f3696
Chinthakindi Bhanu <168004329+bhanu28032004@users.noreply.github.com>
2024-07-19 15:40:45 +0530

    script.js

---
+++ PORTFOLIO/script.js
@@ -0,0 +1,13 @@
+/*what i'm*/
+var typeData = new Typed(".role", {
+    strings: [
+        "Front-end Developer",
+        "Web Developer",
+        "UI-UX Designer",
+        "Java Coder",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1000,
+});
